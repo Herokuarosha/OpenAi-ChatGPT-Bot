238c025177ad2482eecdeb3ba2c7d28b4e1af537
Herokuarosha <135728420+Herokuarosha@users.noreply.github.com>
2023-06-10 10:11:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,16 +9,16 @@ global.OFFLINE = false // if you want put bot offline put true
 global.BOT_PRIVET = false // if you want use bot privet 
 global.AUTO_CHATGPT = false // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
-global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
+global.ONLY_INBOX = true // Only inbox mod ( only pm ) use
 global.ONLY_GROUPS = false // Only Groups Use
 
 
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
+global.OWNER_NAME = 'Arosha Ofc' // Owner Name
+global.OWNER_NUMBER = ['94716141925'] //Owner Number
+global.BOT_ADMINS = ['94716141925','947161419252'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 
 
@@ -27,7 +27,7 @@ global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and 
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
+global.AI_PASSWORD = 'king-nima-gpt-bot'
 
 
 /*
